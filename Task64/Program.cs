9df8baab5ff311c9cfa91981dfe7a47a0345b8bb
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-02 00:52:15 +0300

    Task64 - решение с испр.

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -5,6 +5,13 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+int GetNumberFromUser(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
 void NatureNumbersRevert(int num)
 {
     if (num == 0) return;
@@ -13,9 +20,12 @@ void NatureNumbersRevert(int num)
     NatureNumbersRevert(num - 1);
 }
 
-// Console.Clear();
-Console.Write("Введите число N: ");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+string title = "Вывод натуральных чисел в промежутке от N до 1.\n"
+             + "-----------------------------------------------";
+Console.WriteLine(title);
+
+int number = GetNumberFromUser("Введите число N: ");
 
 Console.Write($"N = {number} -> ");
 NatureNumbersRevert(number);
