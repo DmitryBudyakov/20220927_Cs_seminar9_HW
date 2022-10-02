09215b93d7ae331868811d4ec07f2f05f1a0ea0e
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-02 18:31:32 +0300

    Task68  - исправлены условия

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -13,9 +13,12 @@ int GetNumberFromUser(string msg)
 int AkkermanFunction(int m, int n)
 {
     if (m == 0) return (n + 1);
-    else if (m > 0 && n == 0) return AkkermanFunction(m - 1, 1);
-    else if (m > 0 && n > 0) return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
-    else return 0;
+    else
+        if (m > 0 && n == 0) return AkkermanFunction(m - 1, 1);
+    else
+        // if (m > 0 && n > 0) return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    // else return 0;
 }
 
 Console.Clear();
